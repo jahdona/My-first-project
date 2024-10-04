6bfcc06c239052d3163e3295186ab06547a430af
Jahdona <jahdonah@yahoo.com>
2024-10-04 15:17:56 -0700

    Creating joblisting

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
-
+import {FaMapMarker} from 'react-icons/fa'
+import { useState } from 'react'
 const JobListing = ({job}) => {
+    const [showFullDescription, setShowFullDescription] = useState(false);
+    let description = job.description;
+    if(!showFullDescription)
+    {
+        description = description.substring(0,90) + '...';
+    }
   return (
     <div className='bg-white rounded-xl shadow-md relative '>
     <div className='p-4'>
@@ -12,15 +19,19 @@ const JobListing = ({job}) => {
 
         </div>
         <div className='mb-5'>
-            {job.description}
+            {description}
+           
         </div>
+        <button onClick={()=>setShowFullDescription((prevState)=>!prevState)}className='text-indigo-500 mb-5 hover:text-indigo-600'>
+            { showFullDescription ? '  Less' :'  More'}
+            </button>
         <h3 className='text-indigo-500 mb-2'>
             {job.salary} / Year
         </h3>
         <div className='border border-gray-100 mb-5'></div>
         <div className='flex flex-col lg:flex-row'>
             <div className='text-orange-700 mb-3'>
-                <i className='fa-solid fa-location-dot text-lg'> </i>
+                <FaMapMarker className='inline text-lg mb-1 mr-1'/>
                 {job.location}
             </div>
             <a 
